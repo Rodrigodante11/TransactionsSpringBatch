9f7709a7f885ef94d9f44640a2ebc04f48b3bbed
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2024-06-24 13:40:29 -0300

    first commit

---
+++ src/main/java/com/rodrigo/transactionsspringbatch/TransactionsSpringBatchApplication.java
@@ -0,0 +1,14 @@
+package com.rodrigo.transactionsspringbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionsSpringBatchApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TransactionsSpringBatchApplication.class, args);
+	}
+
+}
